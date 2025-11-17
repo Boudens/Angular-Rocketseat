b0806ce906cf567a55e7f1d73cc649d6bd9529b3
Boudens <caioboudens00@gmail.com>
2025-11-17 17:07:15 -0300

    Criação da pagina de formulario

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,12 @@ import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
+import { CertificadoForm } from './pages/certificado-form/certificado-form';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados,CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
